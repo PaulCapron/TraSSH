PaulCapron/TraSSH

commit e36f25b0352303251c6c6987c56abcf10fdcf775

Don’t poll for clients writability
There is no need: they are either ready to receive more junk data,
or so slow that they deserve to be close()d. We only send 32 bytes
of data every $SLEEP_TIME seconds.

Just record the clients file descriptors, loop over them every round
(every $SLEEP_TIME seconds), and call write(). The write either succeeds
or we close the client, very probably because it closed the connection
on its end. (We don’t have to explicitely care about why/what failed;
we can just log the value of errno and move on.)

A file descriptor is an int, so (on an x64 machine) 4 bytes. That’s a
third of the size of a ‘struct epoll_event’ (12 bytes). Less memory use
is always cool, that also means less pressure on the CPU caches.
And there are now less bytes crossing the kernel/user-space border every
round.

We still have to poll to see when/if they are new clients to accept().
We can simply resort to poll() for that; it’s simpler than epoll(), and
standardized by POSIX (while epoll is Linux specific). That will help
portability to the BSDs (& co.), which is a mid-term goal.

Regarding the clients file descriptors, we store them in a simple array,
statically allocated to its max capacity ($MAX_CLIENTS slots). We did
that before too, with the ‘struct epoll_event’ array. However, we now
have more management to do.
Given a client fd, we compute its index in the array using substraction.
That’s easy but sloppy; an assertion in welcome_new_client prevents out
of bounds access. Worse is better.
The array can be sparse, we manage that using the “tombstone trick”, seen
in some hash tables.
See http://www.craftinginterpreters.com/hash-tables.html#deleting-entries
We also keep track of where the last occupied slot here, using another
special value: “END”, equals to 0. That makes the array initialization
easy, but that’s a bit sloppy too, given that 0 is a valid fd number
(it’s stdin).

Gone is the $COMA_TIME logic: now if $MAX_CLIENTS is reached, we instead

sshtarp.c
@@ -5,7 +5,7 @@
  * this tarpit monologues more of the SSH protocol, fooling more bots.
  * It keeps no client state, and it is not dynamically configurable.
  *
- * Linux-only (epoll). Delegates to systemd socket setup and sandboxing.
+ * Linux-only. Delegates to systemd socket setup and sandboxing.
  * No stdio or stdlib use. Static link with <https://www.musl-libc.org/>.
  *
  * For reference, see "The Secure Shell (SSH) Transport Layer Protocol",
@@ -19,19 +19,23 @@
 
 #define _GNU_SOURCE  /* accept4 */
 #include <netinet/in.h>
-#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <errno.h>
+#include <poll.h>
 #include <stdarg.h>
 #include <string.h>  /* strerror */
+#include <time.h>
 #include <unistd.h>
 
 enum {
-    MAX_EVENTS = 512,  /* max number of events/fds per epoll run */
-    SLEEP_TIME =   9,  /* seconds of rest between epoll runs     */
-    COMA_TIME  = SLEEP_TIME * 3,  /* rest if max events reached  */
+    MAX_CLIENTS = 512,  /* max number of simultaneously connected peers */
+    SLEEP_TIME  =   9,  /* seconds of rest between writes to clients    */
 
-    LISTEN_FD = STDIN_FILENO,  /* server socket, passed by systemd */
+    LISTEN_FD       = STDIN_FILENO,       /* server socket, passed by systemd */
+    FIRST_CLIENT_FD = STDERR_FILENO + 1,  /* first free file descriptor       */
+
+    END =  0,  /* never allocated / terminating marker in the clients array  */
+    RIP = -1,  /* "once allocated, may not be the last occupied slot" marker */
 
     SSH_MSG_KEXINIT     = 20,  /* code for a SSH key-exchange init message */
     SSH_MSG_KEXDH_REPLY = 31   /* Diffie-Hellman key exchange, from server */
@@ -131,20 +135,19 @@ dprintf(int fd, const char *fmt, ...)  /* light version of stdio dprintf */
     return write(fd, buf, j);
 }
 
-static void __attribute__((cold))
+static void __attribute__((cold, noreturn))
 die(const char *msg)
 {
     dprintf(STDERR_FILENO, "%s: %s\n", msg, strerror(errno));
     _exit(2);
 }
 
-static int
-welcome_new_client(int srvfd, int epollfd)
+static void
+welcome_new_client(int srvfd, int clients[])
 {
     struct sockaddr_storage addr;
     socklen_t addrlen;
     int fd;
-    struct epoll_event evt;
     unsigned char *ip;
 
     addrlen = sizeof addr;
@@ -156,13 +159,8 @@ welcome_new_client(int srvfd, int epollfd)
         dprintf(STDOUT_FILENO, "could not greet %u: %s\n",
                 (unsigned)fd, strerror(errno));
         close(fd);
-        return 0;  /* not really welcomed */
+        return;
     }
-
-    evt.events = EPOLLOUT;
-    evt.data.fd = fd;
-    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &evt) == -1)
-        die("epoll_clt");
 
     switch (addr.ss_family) {
     case AF_INET:
@@ -184,60 +182,67 @@ welcome_new_client(int srvfd, int epollfd)
     default:
         dprintf(STDOUT_FILENO, "new con, fd %u\n", fd);
     }
-    return 1;
+
+    if (fd < FIRST_CLIENT_FD || fd >= MAX_CLIENTS - FIRST_CLIENT_FD) {
+        errno = EFAULT;
+        die("last client fd makes for an out of bounds index");
+    }
+    clients[fd - FIRST_CLIENT_FD] = fd;
 }
 
-static void
-handle_client(struct epoll_event evt, const void *data, size_t datalen)
+static int
+handle_clients(int clients[], const void *data, size_t datalen)
 {
-    char *reason;
-    ssize_t written;
-    int fd = evt.data.fd;
+    int i, writes, fd;
 
-    if (evt.events & EPOLLHUP) { reason = "peer closed"; goto close; }
-    if (evt.events & EPOLLERR) { reason = "epoll error"; goto close; }
+    for (i = writes = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i] == END) break;
+        if (clients[i] == RIP) continue;
 
-    written = write(fd, data, datalen);
-    if (written == -1) {
-        reason = strerror(errno);
-        goto close;
-    } else if ((size_t)written < datalen) {
-        reason = "short write";
-        goto close;
+        fd = clients[i];
+        if (write(fd, data, datalen) == (ssize_t)datalen) {
+            writes++;
+        } else {
+            dprintf(STDOUT_FILENO, "closing fd %u: %s\n", fd, strerror(errno));
+            /* errno may just be EWOULDBLOCK, or even 0 in case of short write.
+               Slow clients get nuked, by design. */
+            close(fd);
+            clients[i] = RIP;
+        }
     }
-    return;
- close:
-    dprintf(STDOUT_FILENO, "closing fd %u: %s\n", (unsigned)fd, reason);
-    close(fd);
+
+    if (writes == MAX_CLIENTS) {  /* full; make some room */
+        for (i = 0; i < MAX_CLIENTS; i += (MAX_CLIENTS / 32)) {
+            fd = clients[i];
+            dprintf(STDOUT_FILENO, "closing fd %u: max clients reached\n", fd);
+            close(fd);
+            clients[i] = RIP;
+        }
+    }
+    return writes;
 }
 
 int
 main()
 {
-    int epfd, nevts, i;
-    struct epoll_event evt, evts[MAX_EVENTS];
-
-    epfd = epoll_create1(EPOLL_CLOEXEC);  /* safety close-on-exec */
-    if (epfd == -1)
-        die("epoll_create1");
-
-    evt.events = EPOLLIN;
-    evt.data.fd = LISTEN_FD;
-    if (epoll_ctl(epfd, EPOLL_CTL_ADD, LISTEN_FD, &evt) == -1)
-        die("epoll_ctl(LISTEN_FD)");
+    int clients[MAX_CLIENTS] = { END };
+    struct pollfd pfd[1]     = { { LISTEN_FD, POLLIN, 0 } };
 
     for (;;) {
-        nevts = epoll_wait(epfd, evts, MAX_EVENTS, -1);
-        if (nevts == -1)
-            die("epoll_wait");
+        int timeout;
+        time_t start = time(NULL);
 
-        for (i = 0; i < nevts; i++)
-            if (evts[i].data.fd == LISTEN_FD)
-                welcome_new_client(LISTEN_FD, epfd);
-            else
-                handle_client(evts[i], BOGUS_DATA, sizeof BOGUS_DATA);
+        if (handle_clients(clients, BOGUS_DATA, sizeof BOGUS_DATA) == 0)
+            timeout = -1;  /* no clients; wait for one indefinitely */
+        else
+            timeout = SLEEP_TIME * 1000;
 
-        if (sleep((nevts < MAX_EVENTS) ? SLEEP_TIME : COMA_TIME) != 0)
-            die("sleep");
+        switch(poll(pfd, 1, timeout)) {
+        case -1:
+            die("poll");
+        case 1:
+            welcome_new_client(LISTEN_FD, clients);
+            sleep(time(NULL) - start);
+        }
     }
 }

sshtarp.service
@@ -38,7 +38,6 @@ DevicePolicy=strict
 InaccessibleDirectories=/bin /boot /etc /lib /lib64 /mnt /media
 InaccessibleDirectories=/opt /proc /sbin /srv /sys /tmp /usr
 
-SystemCallFilter=epoll_create1 epoll_ctl epoll_pwait
-SystemCallFilter=accept4 write close
+SystemCallFilter=poll accept4 write close
 SystemCallFilter=arch_prctl
 SystemCallArchitectures=native

commit 7466991906799f5fdbef755587123cca5f39ce4f

Desynchronize write() and accept()
Previously in every round of the “main infinite loop”, we were doing:
  — a write($BOGUS_DATA) to every established clients
  — an accept() of any queued (new) client
And the interval between rounds was $SLEEP_TIME seconds.

(“Clients”, here & in general in this project, mean “malevolent bots”.)

But that implied conflicting requirements on what $SLEEP_TIME should be:
  — the interval between writes should be as high as possible, to keep
  the clients stuck for as long as possible, with the least amount of
  work for us. We just don’t want to trigger a timeout on their side.
  — on the other hand, we want to troll as many clients as sensible, so
  we want the interval between accepts() to be quite low. Moreover, I
  suspect that many clients don’t wait much to see if they can connect,
  even if they tolerate, after that, much longer delays.

So instead of settling on a “middle-ground” $SLEEP_TIME that does not
really satisfy anything, this commit replaces it by two independent
values, $WRITES_INTERVAL and $ACCEPTS_INTERVAL. And in a loop round, we
now do either a “write pass”, or an “accept pass”, or, sometimes, both.

--

I was dreadful this change was going to make the codebase transition
from “obviously error-free” to “no obvious error”*. But it turns out
that after some thinking and trials, a simple, clear solution could be
devised. One key is to deal as much as possible in absolute times.

By the way:
 “Resist the temptation to implement a sleep for a fixed amount of time
  by using the return value of sleep, when nonzero, to call sleep again.
  This will work with a certain amount of accuracy as long as signals
  arrive infrequently. […]
  Instead, compute the calendar time at which the program should stop
  waiting, and keep trying to wait until that calendar time. This won’t
  be off by more than a second.”
 — https://www.gnu.org/software/libc/manual/html_node/Sleeping.html

(We are, thankfully, not concerned by signals; we don’t handle them,
and let the program exits if it receives any. Signals stink.)

--

There was a mistake in time keeping previously, introduced in e36f25b03:

sshtarp.c
@@ -28,8 +28,9 @@
 #include <unistd.h>
 
 enum {
-    MAX_CLIENTS = 512,  /* max number of simultaneously connected peers */
-    SLEEP_TIME  =   9,  /* seconds of rest between writes to clients    */
+    MAX_CLIENTS      = 512,  /* max number of simultaneously connected peers  */
+    WRITES_INTERVAL  =  18,  /* seconds of rest between writes to clients     */
+    ACCEPTS_INTERVAL =   5,  /* seconds of rest between accepts of new peers  */
 
     LISTEN_FD       = STDIN_FILENO,       /* server socket, passed by systemd */
     FIRST_CLIENT_FD = STDERR_FILENO + 1,  /* first free(able) file descriptor */
@@ -140,6 +141,21 @@ die(const char *msg)
 {
     dprintf(STDERR_FILENO, "%s: %s\n", msg, strerror(errno));
     _exit(2);
+}
+
+static void
+sleeptill(time_t deadline)
+{
+    struct timespec t;
+    int r;
+
+    t.tv_sec = deadline;
+    t.tv_nsec = 0;
+    r = clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &t, NULL);
+    if (r != 0) {
+        errno = r;  /* cannot be EINTR: no signals are handled */
+        die("clock_nanosleep");
+    }
 }
 
 static void
@@ -254,24 +270,34 @@ main()
 {
     int clients[MAX_CLIENTS] = { END };
     struct pollfd pfd[1]     = { { LISTEN_FD, POLLIN, 0 } };
+    time_t nextwrite         = 0;
+    time_t nextaccept        = 0;
 
     clean_file_descriptors();
 
     for (;;) {
         int timeout;
-        time_t start = time(NULL);
+        time_t now = time(NULL);
 
-        if (handle_clients(clients, BOGUS_DATA, sizeof BOGUS_DATA) == 0)
-            timeout = -1;  /* no clients; wait for one indefinitely */
-        else
-            timeout = SLEEP_TIME * 1000;
-
-        switch(poll(pfd, 1, timeout)) {
-        case -1:
-            die("poll");
-        case 1:
-            welcome_new_client(LISTEN_FD, clients);
-            sleep(time(NULL) - start);
+        if (now >= nextwrite) {
+            nextwrite = now + WRITES_INTERVAL;
+            if (handle_clients(clients, BOGUS_DATA, sizeof BOGUS_DATA) == 0)
+                timeout = -1;  /* no clients; wait for one indefinitely */
+            else
+                timeout = WRITES_INTERVAL * 1000;
+        } else {
+            timeout = (nextwrite - now) * 1000;
         }
+
+        if (now >= nextaccept)
+            switch(poll(pfd, 1, timeout)) {
+            case -1: die("poll");
+            case 0: continue;  /* timed out; no need to sleep */
+            case 1:
+                welcome_new_client(LISTEN_FD, clients);
+                nextaccept = time(NULL) + ACCEPTS_INTERVAL;
+            }
+
+        sleeptill((nextaccept < nextwrite) ? nextaccept : nextwrite);
     }
 }